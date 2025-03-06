438de4edfaf83c9ae15d92873a4a26805fdc3eed
key-moon <kymn0116@gmail.com>
2025-03-07 01:50:58 +0900

    add quiet mode to runner

--- bllind/runner.py
+++ bllind/runner.py
@@ -2,20 +2,25 @@ from typing import Callable
 from .predictor import Predictor
 from .query_planner import QueryPlanner
 
-def do_blind_attack(oracle: Callable, predictor: Predictor, queryplanner: QueryPlanner, known_prefix: str, known_suffix: str):
+def do_blind_attack(oracle: Callable, predictor: Predictor, queryplanner: QueryPlanner, known_prefix: str, known_suffix: str, logging=True):
   prefix = known_prefix
-  print(f"Initial prefix: {prefix}")
+  if logging:
+    print(f"Initial prefix: {prefix}")
   while not prefix.endswith(known_suffix):
     probs = predictor.predict(prefix)
-    print({k: round(v, 4) for k, v in sorted(probs.items(), key=lambda item: item[1], reverse=True)})
+    if logging:
+      print({k: round(v, 4) for k, v in sorted(probs.items(), key=lambda item: item[1], reverse=True)})
     while 1 < len(probs):
       query, post_probs = queryplanner.plan(probs)
-      print(f"{query=}, {len(probs)=}")
+      if logging:
+        print(f"{query=}, {len(probs)=}")
       result = oracle(prefix, query)
       # print(f"Oracle result: {result}")
       probs = post_probs[result]
     next_char = list(probs.keys())[0]
-    print(f"Next character to add: {next_char}")
+    if logging:
+     print(f"Next character to add: {next_char}")
     prefix += next_char
-    print(f"Updated prefix: {prefix}")
+    if logging:
+      print(f"Updated prefix: {prefix}")
   return prefix
