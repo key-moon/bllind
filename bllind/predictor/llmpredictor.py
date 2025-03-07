62ebf68df8546e59bea74436d5c832569300cf27
key-moon <kymn0116@gmail.com>
2025-03-07 16:59:18 +0900

    add kv-cache and small bugfix

--- bllind/predictor/llmpredictor.py
+++ bllind/predictor/llmpredictor.py
@@ -1,3 +1,4 @@
+from functools import cache
 import hashlib
 from pathlib import Path
 from typing import Callable, Dict, Union
@@ -11,6 +12,7 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import re
 import pickle
+import time
 
 CTF_FLAG_PROMPT = "A CTF Flag usually forms a sentence encoded in a 1337 form, and words are joined with `-` or `_`. Following is a example: {prefix}"
 DEFAULT_MODEL = "openai-community/gpt2"
@@ -54,33 +56,41 @@ def get_cache_path(
   return user_cache_dir
 
 
+cached_model_retriever = cache(AutoModelForCausalLM.from_pretrained)
+cached_tokenizer_retriever = cache(AutoTokenizer.from_pretrained)
+
+DEBUG_CACHE = True
+
+
 class LLMPredictor(Predictor):
   def __init__(
     self,
     prompt: str | Callable[[str], str],
     model: str = DEFAULT_MODEL,
-    permanent_attention_cache: Union[bool, str] = False,
+    use_attention_cache: bool = True,
     permanent_next_token_cache: Union[bool, str] = False,
   ):
     self.promptGetter = (
       prompt if callable(prompt) else lambda prefix: prompt.format(prefix=prefix)
     )
-    self.model = AutoModelForCausalLM.from_pretrained(model)
+    self.model = cached_model_retriever(model)
     self.model.eval()
-    self.tokenizer = AutoTokenizer.from_pretrained(model)
+    self.tokenizer = cached_tokenizer_retriever(model)
 
     self.token_count = len(self.tokenizer)
     self.tokens = self.tokenizer.convert_ids_to_tokens(list(range(self.token_count)))
 
-    self.attention_cache_path = get_cache_path(
-      permanent_attention_cache, "attention", prompt, model
-    )
     self.next_token_cache_path = get_cache_path(
       permanent_next_token_cache, "next_token", prompt, model
     )
 
+    self.use_attention_cache = use_attention_cache
+    self.past_key_values: Dict[str, Dict[str, float]] = {}
+
     self.next_tokens: Dict[str, Dict[str, float]] = {}
     self.next_char_probs_dict: Dict[str, Dict[str, float]] = {}
+
+    self.token_decoder: Callable[[str], str] = lambda s: s.replace("Ġ", " ")
 
   def _get_permanent_next_token_cache(self, prefix: str):
     if self.next_token_cache_path is None:
@@ -88,6 +98,7 @@ class LLMPredictor(Predictor):
     path = self.next_token_cache_path / hashlib.md5(prefix.encode()).hexdigest()
     if not path.exists():
       return None
+    print("[*] permanent next_token cache hit!")
     with path.open("rb") as f:
       return pickle.load(f)
 
@@ -108,14 +119,39 @@ class LLMPredictor(Predictor):
 
     next_token_probs = self._get_permanent_next_token_cache(prefix)
     if next_token_probs is None:
+      input_ids = inputs["input_ids"]
+      if self.use_attention_cache:
+        prev_input = "".join(
+          map(
+            self.token_decoder, self.tokenizer.convert_ids_to_tokens(input_ids[0][:-1])
+          )
+        )
+        past_kv = self.past_key_values.get(prev_input)
+        print(f"[*] has attention cache: {past_kv is not None}")
+      else:
+        past_kv = None
       with torch.no_grad():
-        outputs = self.model(**inputs)
+        start_time = time.time()
+        if past_kv is None:
+          outputs = self.model(**inputs)
+        else:
+          outputs = self.model(
+            input_ids[:, -1:],
+            past_key_values=past_kv,
+          )
+          # if DEBUG_CACHE:
+          # assert self.model(**inputs).logits == outputs.logits
+        end_time = time.time()
+        print(f"[*] Time taken for prediction: {end_time - start_time:.2f} secs")
+        if self.use_attention_cache:
+          self.past_key_values[prompt] = outputs.past_key_values
         probabilities = torch.softmax(outputs.logits[:, -1, :], dim=-1)
         next_token_probs = probabilities[0].cpu().numpy()
       self._set_permanent_next_token_cache(prefix, next_token_probs)
 
     cur_next_token = {
-      token: float(prob) for token, prob in zip(self.tokens, next_token_probs)
+      self.token_decoder(token): float(prob)
+      for token, prob in zip(self.tokens, next_token_probs)
     }
     self.next_tokens[prefix] = cur_next_token
 
